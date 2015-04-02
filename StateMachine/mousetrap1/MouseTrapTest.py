1155f2f5e8b310a7d53e7206e69221cc44d8cb5d
vanhong <vanhongkao@gmail.com>
2015-04-02 11:33:34 +0800

    finished using if to change state

---
+++ StateMachine/mousetrap1/MouseTrapTest.py
@@ -0,0 +1,67 @@
+# StateMachine/mousetrap1/MouseTrapTest.py
+# State Machine pattern using 'if' statements
+# to determine the next state
+
+import string, sys
+sys.path += ['../', '../mouse']
+print sys.path
+from State import State
+from StateMachine import StateMachine
+from MouseAction import MouseAction
+# A different subclass fro each state:
+
+class Waiting(State):
+	def run(self):
+		print("Waiting: Broadcasting cheese small")
+
+	def next(self, input):
+		if input == MouseAction.appears:
+			return MouseTrap.luring
+		return MouseTrap.waiting
+
+class Luring(State):
+	def run(self):
+		print("Luring: Presenting Cheese, door open")
+
+	def next(self, input):
+		if input == MouseAction.runsAway:
+			return MouseTrap.waiting
+		if input == MouseAction.enters:
+			return MouseTrap.trapping
+		return MouseTrap.luring
+
+class Trapping(State):
+	def run(self):
+		print("Trapping: Closing door")
+
+	def next(self, input):
+		if input == MouseAction.escapes:
+			return MouseTrap.waiting
+		if input == MouseAction.trapped:
+			return MouseTrap.holding
+		return Mouse.trapping
+
+class Holding(State):
+	def run(self):
+		print("Holding: Mouse caught")
+
+	def next(self, input):
+		if input == MouseAction.removed:
+			return MouseTrap.waiting
+		return MouseTrap.holding
+
+class MouseTrap(StateMachine):
+	def __init__(self):
+		# Initial state
+		StateMachine.__init__(self, MouseTrap.waiting)
+
+# Static variable initialization
+MouseTrap.waiting = Waiting()
+MouseTrap.luring = Luring()
+MouseTrap.trapping = Trapping()
+MouseTrap.holding = Holding()
+
+moves = map(string.strip, open("../mouse/MouseMoves.txt").readlines())
+MouseTrap().runAll(map(MouseAction, moves))
+
+
